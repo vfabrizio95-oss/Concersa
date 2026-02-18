dd1aecc1068b3b3ecc4dcdc82651e4e33e2e4edf
vfabrizio95 <vfabrizio95@gmail.com>
2026-02-18 18:34:52 -0500

    Se actualizo el archivo SQS: Se migro de SQS a FIFO con KMS e integracion SNS

--- sqs.tf
+++ sqs.tf
@@ -1,158 +1,152 @@
-resource "aws_sqs_queue" "orden_recibida" {
-  name                       = "${var.project_name}-orden-recibida-${var.environment}"
-  delay_seconds              = 0
-  max_message_size           = 262144
-  message_retention_seconds  = 345600
-  receive_wait_time_seconds  = 10
-  visibility_timeout_seconds = 300
-
-    sqs_managed_sse_enabled = true
+resource "aws_sqs_queue" "valorizaciones" {
+  name                        = "${local.prefix}-valorizaciones.fifo"
+  fifo_queue                  = true
+  content_based_deduplication = true
+  visibility_timeout_seconds  = 300
+  kms_master_key_id           = aws_kms_key.main.id
 
   redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.orden_recibida_dlq.arn
+    deadLetterTargetArn = aws_sqs_queue.valorizaciones.arn
     maxReceiveCount     = 3
   })
 
   tags = {
-    Name = "${var.project_name}-orden-recibida"
+    Name = "${local.prefix}-sqs-valorizaciones"
   }
 }
 
 resource "aws_sqs_queue" "orden_recibida_dlq" {
-  name                      = "${var.project_name}-orden-recibida-dlq-${var.environment}"
-  message_retention_seconds = 1209600 # 14 días
+  name                      = "${local.prefix}-valorizaciones-dlq.fifo"
+  fifo_queue                = true
+  message_retention_seconds = 1209600
+  kms_master_key_id         = aws_kms_key.main.id
 
-    sqs_managed_sse_enabled = true
+  tags = {
+    Name = "${local.prefix}-valorizaciones-dlq"
+  }
 }
 
-resource "aws_sqs_queue" "orden_validada" {
-  name                       = "${var.project_name}-orden-validada-${var.environment}"
-  delay_seconds              = 0
-  max_message_size           = 262144
-  message_retention_seconds  = 345600
-  receive_wait_time_seconds  = 10
-  visibility_timeout_seconds = 300
-
-    sqs_managed_sse_enabled = true
+resource "aws_sqs_queue" "ordenes" {
+  name                        = "${local.prefix}-ordenes.fifo"
+  fifo_queue                  = true
+  content_based_deduplication = true
+  visibility_timeout_seconds  = 300
+  kms_master_key_id           = aws_kms_key.main.id
 
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.orden_validada_dlq.arn
     maxReceiveCount     = 3
   })
-}
 
-resource "aws_sqs_queue" "orden_validada_dlq" {
-  name                      = "${var.project_name}-orden-validada-dlq-${var.environment}"
-  message_retention_seconds = 1209600
-
-    sqs_managed_sse_enabled = true
-}
-
-resource "aws_sqs_queue" "orden_ejecutada" {
-  name                       = "${var.project_name}-orden-ejecutada-${var.environment}"
-  delay_seconds              = 0
-  max_message_size           = 262144
-  message_retention_seconds  = 345600
-  receive_wait_time_seconds  = 10
-  visibility_timeout_seconds = 300
-
-    sqs_managed_sse_enabled = true
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.orden_ejecutada_dlq.arn
-    maxReceiveCount     = 3
-  })
-}
-
-resource "aws_sqs_queue" "orden_ejecutada_dlq" {
-  name                      = "${var.project_name}-orden-ejecutada-dlq-${var.environment}"
-  message_retention_seconds = 1209600
-
-    sqs_managed_sse_enabled = true
-}
-
-resource "aws_sqs_queue_policy" "orden_recibida" {
-  queue_url = aws_sqs_queue.orden_recibida.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "events.amazonaws.com"
-        }
-        Action   = "sqs:SendMessage"
-        Resource = aws_sqs_queue.orden_recibida.arn
-        Condition = {
-          ArnEquals = {
-            "aws:SourceArn" = aws_cloudwatch_event_rule.orden_recibida.arn
-          }
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_sqs_queue_policy" "orden_validada" {
-  queue_url = aws_sqs_queue.orden_validada.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "events.amazonaws.com"
-        }
-        Action   = "sqs:SendMessage"
-        Resource = aws_sqs_queue.orden_validada.arn
-        Condition = {
-          ArnEquals = {
-            "aws:SourceArn" = aws_cloudwatch_event_rule.orden_validada.arn
-          }
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_sqs_queue_policy" "orden_ejecutada" {
-  queue_url = aws_sqs_queue.orden_ejecutada.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "events.amazonaws.com"
-        }
-        Action   = "sqs:SendMessage"
-        Resource = aws_sqs_queue.orden_ejecutada.arn
-        Condition = {
-          ArnEquals = {
-            "aws:SourceArn" = aws_cloudwatch_event_rule.orden_ejecutada.arn
-          }
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_cloudwatch_metric_alarm" "orden_recibida_dlq" {
-  alarm_name          = "${var.project_name}-orden-recibida-dlq-alarm-${var.environment}"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = "1"
-  metric_name         = "ApproximateNumberOfMessagesVisible"
-  namespace           = "AWS/SQS"
-  period              = "300"
-  statistic           = "Average"
-  threshold           = "1"
-  alarm_description   = "Alerta cuando hay mensajes en la DLQ"
-  treat_missing_data  = "notBreaching"
-
-  dimensions = {
-    QueueName = aws_sqs_queue.orden_recibida_dlq.name
+  tags = {
+    Name = "${local.prefix}-sqs-ordenes"
   }
+}
+
+resource "aws_sqs_queue" "ordenes_dlq" {
+  name                      = "${local.prefix}-ordenes-dlq.fifo"
+  fifo_queue                = true
+  message_retention_seconds = 1209600
+  kms_master_key_id         = aws_kms_key.main.id
+
+  tags = {
+    Name = "${local.prefix}-ordenes-dlq"
+  }
+}
+
+resource "aws_sns_topic" "notificaciones" {
+  name              = "${local.prefix}-notificaciones"
+  kms_master_key_id = aws_kms_key.main.id
+
+  tags = {
+    Name = "${local.prefix}-sns-notificaciones"
+  }
+}
+
+resource "aws_sns_topic" "valorizacion_terminada" {
+  name              = "${local.prefix}-valorizacion-terminada"
+  kms_master_key_id = aws_kms_key.main.id
+
+  tags = {
+    Name = "${local.prefix}-sns-valorizacion"
+  }
+}
+
+resource "aws_sns_topic_subscription" "valorizacion_a_sqs" {
+  topic_arn            = aws_sns_topic.valorizacion_terminada.arn
+  protocol             = "sqs"
+  endpoint             = aws_sqs_queue.valorizaciones.arn
+  raw_message_delivery = true
+}
+
+resource "aws_sqs_queue_policy" "valorizaciones_sns" {
+  queue_url = aws_sqs_queue.valorizaciones.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "sns.amazonaws.com"
+        }
+        Action   = "sqs:SendMessage"
+        Resource = aws_sqs_queue.valorizaciones.arn
+        Condition = {
+          ArnEquals = {
+            "aws:SourceArn" = aws_sns_topic.valorizacion_terminada.arn
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_sqs_queue_policy" "valorizaciones_eventbridge" {
+  queue_url = aws_sqs_queue.valorizaciones.url
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "events.amazonaws.com"
+        }
+        Action   = "sqs:SendMessage"
+        Resource = aws_sqs_queue.valorizaciones.arn
+        Condition = {
+          ArnEquals = {
+            "aws:SourceArn" = aws_cloudwatch_event_rule.valorizacion_creada.arn
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_sqs_queue_policy" "ordenes_eventbridge" {
+  queue_url = aws_sqs_queue.ordenes.url
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "events.amazonaws.com"
+        }
+        Action   = "sqs:SendMessage"
+        Resource = aws_sqs_queue.ordenes.arn
+        Condition = {
+          ArnEquals = {
+            "aws:SourceArn" = [
+            aws_cloudwatch_event_rule.orden_recibida.arn,
+            aws_cloudwatch_event_rule.orden_eliminada.arn
+            ]
+          }
+        }
+      }
+    ]
+  })
 }
