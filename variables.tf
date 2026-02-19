7c9b67684f96145c24a78182473a37ad2c79965d
vfabrizio95 <vfabrizio95@gmail.com>
2026-02-18 21:30:19 -0500

    Se creo el archivo networking: agrega VPC multi-AZ con subnets publicas y privadas, Nat y endpoints privados

--- variables.tf
+++ variables.tf
@@ -43,6 +43,16 @@ variable "cognito_callback_urls" {
   type = list(string)
 }
 
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "azs" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b"]
+}
+
 locals {
   prefix = "${var.project_name}-${var.environment}"
 }
